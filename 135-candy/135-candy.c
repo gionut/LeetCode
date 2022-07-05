5f98197f2126720fd838365852c92952bc8be6dc
Groza Ionuț <59391450+GIonut@users.noreply.github.com>
2022-07-05 16:17:44 +0300

    Time: 38 ms (45.45%), Space: 7.1 MB (75.76%) - LeetHub

---
+++ 135-candy/135-candy.c
@@ -0,0 +1,48 @@
+int candy(int* ratings, int ratingsSize) {
+    if(ratingsSize == 1) return 1;
+    
+    int candies = 0;
+    int candiesToLastChild = 0;
+    int current = 0;
+    while(current < ratingsSize-1) {
+        int terrain = ratings[current+1] - ratings[current];
+        int steps = stepsUntilChangeInTerrain(ratings, ratingsSize, current, terrain);
+        current += steps-1;
+        
+        if(isPlateau(terrain)) {
+            candies += steps - min(candiesToLastChild, 1);
+            candiesToLastChild = 1;
+        }
+        else {
+            candies += (steps+1)*steps/2 - min(candiesToLastChild, steps);
+            candiesToLastChild = isAscending(terrain)? steps: 1;
+        }
+    }
+    return candies;
+}
+
+int stepsUntilChangeInTerrain(int* array, int arraySize, int startingPosition, int terrain) {
+    int steps = 1;
+    int currentPosition = startingPosition+1;
+    while(currentPosition < arraySize){
+        int currentTerrain = array[currentPosition] - array[currentPosition-1];
+        if(!((terrain == currentTerrain) && (terrain == 0) || terrain*currentTerrain > 0))
+            break;
+        currentPosition++;
+        steps++;
+    }
+    
+    return steps;
+}
+
+int isPlateau(terrain) {
+    return terrain == 0;
+}
+
+int isAscending(terrain) {
+    return terrain > 0;
+}
+
+int min(int a, int b) {
+    return a <= b? a: b; 
+}

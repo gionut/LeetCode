d6a8b38d01a38b48e26dcc657733a4d72f4995d4
Groza Ionuț <59391450+GIonut@users.noreply.github.com>
2022-07-06 14:45:19 +0300

    Time: 3 ms (65.79%), Space: 5.4 MB (66.16%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.c
+++ 509-fibonacci-number/509-fibonacci-number.c
@@ -2,9 +2,10 @@ int fib(int n){
     if(n == 0) return 0;
     int prev = 1;
     int lastPrev = 0;
-    for(int i = 1; i < n; i++) {
+    while(n > 1){
         prev = prev + lastPrev;
         lastPrev = prev - lastPrev;
+        n--;
     }
     return prev;
 }

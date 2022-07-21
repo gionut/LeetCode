7a145080d97b948126ab21c847cc0cf2e8acbdba
Groza Ionuț <59391450+GIonut@users.noreply.github.com>
2022-07-21 12:00:48 +0300

    Time: 7358 ms (6.85%), Space: 95.1 MB (53.09%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        root = tuple(points[0])
+        
+        points = set(map(tuple, points))
+        
+        computeWeight = lambda p1, p2: abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+        getNeighbours = lambda p1: points - set(p1)
+        
+        mst = []
+        visited = set()
+        
+        min_cost = 0
+        
+        min_weight = [(0, (root, root))]
+        
+        while len(min_weight):
+            weight, (source, dest) = heappop(min_weight)
+            if dest not in visited:
+                min_cost += weight 
+                visited.add(dest)
+                new_source = dest
+                neighbours = getNeighbours(new_source) - visited
+                for neighbour in neighbours:
+                    weight = (computeWeight(new_source, neighbour), (new_source, neighbour))
+                    heappush(min_weight, weight)
+        
+        return min_cost
+        

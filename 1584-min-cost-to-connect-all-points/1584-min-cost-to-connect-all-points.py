6e3fc3c114f4f6acce9e96ccb8b974dfbf20ad4c
Groza Ionuț <59391450+GIonut@users.noreply.github.com>
2022-07-21 12:12:13 +0300

    Time: 7956 ms (5.01%), Space: 106.4 MB (47.20%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,29 +1,24 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        root = tuple(points[0])
-        
-        points = set(map(tuple, points))
-        
         computeWeight = lambda p1, p2: abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-        getNeighbours = lambda p1: points - set(p1)
+        getNeighbours = lambda p1: [p for p in range(len(points)) if p != p1 and p not in visited]
         
-        mst = []
         visited = set()
-        
+
         min_cost = 0
         
-        min_weight = [(0, (root, root))]
+        min_weight = [(0, (0, 0))]
         
         while len(min_weight):
-            weight, (source, dest) = heappop(min_weight)
+            weight, (src, dest) = heappop(min_weight)
             if dest not in visited:
                 min_cost += weight 
                 visited.add(dest)
-                new_source = dest
-                neighbours = getNeighbours(new_source) - visited
-                for neighbour in neighbours:
-                    weight = (computeWeight(new_source, neighbour), (new_source, neighbour))
-                    heappush(min_weight, weight)
+                src = dest
+                for dest in range(len(points)):
+                    if dest != src and dest not in visited:
+                        weight = (computeWeight(points[src], points[dest]), (src, dest))
+                        heappush(min_weight, weight)
         
         return min_cost
         

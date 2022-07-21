4005c1ad0956679faf0d008b2a09a8547d5300ef
Groza Ionuț <59391450+GIonut@users.noreply.github.com>
2022-07-21 12:22:36 +0300

    Time: 2799 ms (56.70%), Space: 109.9 MB (45.00%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,24 +1,50 @@
 class Solution:
-    def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        computeWeight = lambda p1, p2: abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-        getNeighbours = lambda p1: [p for p in range(len(points)) if p != p1 and p not in visited]
+    def minCostConnectPoints(self, p: List[List[int]]) -> int:
+        def manhattan(x, y):
+            return abs(x[0]-y[0]) + abs(x[1]-y[1])
         
-        visited = set()
+        ans, n = 0, len(p)
+        seen = set()
+        vertices = [(0, (0, 0))]
 
-        min_cost = 0
+        while len(seen) < n:
+            # print(vertices, seen)
+            w, (u, v) = heapq.heappop(vertices)            
+            if v in seen: continue
+            ans += w
+            seen.add(v)
+            for j in range(n):
+                if j not in seen and j!=v:
+                    heapq.heappush(vertices, (manhattan(p[j], p[v]), (v, j)))
         
-        min_weight = [(0, (0, 0))]
+        return ans
+#         computeWeight = lambda p1, p2: abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+#         getNeighbours = lambda p1: [p for p in range(len(points)) if p != p1 and p not in visited]
         
-        while len(min_weight):
-            weight, (src, dest) = heappop(min_weight)
-            if dest not in visited:
-                min_cost += weight 
-                visited.add(dest)
-                src = dest
-                for dest in range(len(points)):
-                    if dest != src and dest not in visited:
-                        weight = (computeWeight(points[src], points[dest]), (src, dest))
-                        heappush(min_weight, weight)
+#         weights = []
+#         for i in range(len(points)):
+#             for j in range(i+1, len(points)):
+#                 weight = computeWeight(points[i], points[j]) 
+#                 weights[i] = weight
+#                 weights[j][i] = weight
+                
+#         visited = set()
+
+#         min_cost = 0
         
-        return min_cost
+#         min_weight = [(0, (0, 0))]
+        
+#         while len(min_weight):
+#             weight, (src, dest) = heappop(min_weight)
+#             if dest not in visited:
+#                 min_cost += weight 
+#                 visited.add(dest)
+#                 src = dest
+#                 for dest in range(len(points)):
+#                     if dest != src and dest not in visited:
+#                         edge = (src, dest)
+#                         weight = (weights[edge], edge)
+#                         heappush(min_weight, weight)
+        
+#         return min_cost
         

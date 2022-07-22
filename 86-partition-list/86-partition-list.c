75d77e75ab06073dc7fb5ff0efe987abae9bbe8b
Groza Ionuț <59391450+GIonut@users.noreply.github.com>
2022-07-22 23:15:25 +0300

    Time: 6 ms (43.43%), Space: 5.7 MB (97.98%) - LeetHub

---
+++ 86-partition-list/86-partition-list.c
@@ -0,0 +1,82 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+void printList(struct ListNode* head) {
+    struct ListNode* crt = head; 
+    while(crt != NULL) {
+        printf("%d ", crt->val);
+        crt = crt->next;
+    }
+    printf("\n");
+}
+
+
+struct ListNode* getPriorPivot(struct ListNode* head, int x){
+    struct ListNode* priorPivot = NULL;
+    
+    struct ListNode* crt = head;
+    while(crt != NULL) {
+         if(priorPivot == NULL && crt->next != NULL && crt->next->val >= x) {
+            priorPivot = crt;
+        }
+        crt = crt->next;
+    }
+    
+    return priorPivot;
+}
+
+struct ListNode* removeNextAndReturn(struct ListNode* prior) {
+    struct ListNode* aux;
+    aux = prior->next;
+    
+    prior->next = prior->next->next;
+        
+    return aux;
+}
+
+void insertAfter(struct ListNode* node, struct ListNode* toInsert) {
+    toInsert->next = node->next;
+    node->next = toInsert;
+}
+
+int hasNext(struct ListNode* node) {
+    if(node->next != NULL)
+        return 1;
+    return 0;
+}
+
+struct ListNode* partition(struct ListNode* head, int x){
+    struct ListNode helperNode;
+    helperNode.next = head;
+    helperNode.val = 999;
+    
+    struct ListNode* priorPivot = getPriorPivot(&helperNode, x);
+    if(priorPivot == NULL)
+        return head;
+    
+    struct ListNode* crt = priorPivot;
+    
+    while(crt != NULL) {
+       if(hasNext(crt) == 1 && crt->next->val < x) {
+           struct ListNode* removed = removeNextAndReturn(crt);
+           insertAfter(priorPivot, removed);
+           priorPivot = removed;
+           
+           // printList(&helperNode);
+           
+        }
+        else
+            crt = crt->next;    
+    }
+    
+    if(priorPivot == NULL)
+        return head;
+    
+    
+    return helperNode.next;
+}

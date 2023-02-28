c6bf26a26a21f90aba1db25995f140a0fb3f7977
Groza Ionuț <59391450+GIonut@users.noreply.github.com>
2023-02-28 22:33:02 +0200

    Time: 37 ms (50.96%), Space: 13.9 MB (44.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -6,8 +6,6 @@ class Solution:
     def f(self, n):
         if n in self.memo:
             return self.memo[n]
-        two = self.memo[n-2] if n-2 in self.memo else self.f(n-2)
-        one = self.memo[n-1] if n-1 in self.memo else self.f(n-1)
-        result = one + two
+        result = self.f(n-2) + self.f(n-1)
         self.memo[n] = result 
         return result

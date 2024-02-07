c7a746af116c40e7dff6ae764949d3b746c3c127
Groza Ionuț <59391450+gionut@users.noreply.github.com>
2024-02-07 14:45:22 +0200

    Time: 40 ms (87.36%), Space: 18 MB (66.85%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,24 @@
+# "loveleetcode"
+# l-2
+# o-2
+# v-1
+# e-4
+# t-1
+# c-1
+# d-1
+    
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        freq = {}
+        
+        for char in s:
+            if char not in freq:
+                freq[char] = 0
+            freq[char] += 1
+        
+        sorted_keys = sorted(freq.keys(), key=lambda x: freq[x], reverse=True)
+        result = ''
+        for key in sorted_keys:
+            result += key * freq[key]
+        return result

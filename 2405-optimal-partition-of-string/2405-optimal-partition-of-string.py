01d61e735a38d50e6f3d1b79acf49298d946c985
Groza Ionuț <59391450+gionut@users.noreply.github.com>
2024-02-26 21:25:38 +0200

    Time: 106 ms (39.83%), Space: 17.4 MB (57.74%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,12 +1,9 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        binary = [2**i for i in range(26+1)]
-        # print([bin(i) for i in binary])
-        
         count = 1
         crt_partition = 0
         for c in s:
-            c_bin = binary[ord(c)-ord('a')]
+            c_bin = 1 << (ord(c)-ord('a'))
             if  c_bin | crt_partition != crt_partition:
                 crt_partition = c_bin | crt_partition
             else:

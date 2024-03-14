795b80cb7a2e137de2d718c22d3fcb7f2b925549
Groza Ionuț <59391450+gionut@users.noreply.github.com>
2024-03-14 20:29:30 +0200

    Time: 977 ms (5.29%), Space: 300.2 MB (6.61%) - LeetHub

---
+++ 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
@@ -0,0 +1,27 @@
+# if len(array) < 2:
+#     return 0
+# check the ends:
+#     if equal:
+#         return recurse for the array without the ends
+#     else:
+#         return min(
+#             1 + recurse with left merged array,
+#             1 + recurse with right merged array
+#         )
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        @lru_cache(maxsize=2**19)
+        def minOps(start, end, start_idx, end_idx):
+            if end_idx - start_idx < 1:
+                return 0
+            
+            if start < end:
+                return 1 + minOps(start+nums[start_idx+1], end, start_idx+1, end_idx)
+            
+            elif end < start:
+                return 1 + minOps(start, end+nums[end_idx-1], start_idx, end_idx-1)
+            else:
+                return minOps(nums[start_idx+1], nums[end_idx-1], start_idx+1, end_idx-1)
+
+        return minOps(nums[0], nums[-1], 0, len(nums)-1)

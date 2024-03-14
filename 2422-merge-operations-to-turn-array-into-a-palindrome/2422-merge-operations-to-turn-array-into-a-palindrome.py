d39bb5e1b15ad234fe93fe22087d50b33e21d477
Groza Ionuț <59391450+gionut@users.noreply.github.com>
2024-03-14 20:30:04 +0200

    Time: 941 ms (5.60%), Space: 137.4 MB (6.61%) - LeetHub

--- 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
+++ 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
@@ -11,7 +11,7 @@
 
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        # @lru_cache(maxsize=2**16)
+        @lru_cache(maxsize=256)
         def minOps(start, end, start_idx, end_idx):
             if end_idx - start_idx < 1:
                 return 0

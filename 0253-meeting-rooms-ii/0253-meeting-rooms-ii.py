bf9dd622c0d958447fc14920028a2f7324471138
Groza Ionuț <59391450+gionut@users.noreply.github.com>
2024-03-15 16:07:29 +0200

    Time: 82 ms (18.02%), Space: 19.9 MB (60.75%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,24 +1,7 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-#         sort
-#         pq
-#         foreach interval:
-#             ends_soon = pq.peek()
-#             if interval.end > ends_soon:
-#                 pq.pop()
-#                 pq.push(interval.end)
-#             else:
-#                 pq.push(interval.end)
-                    
-#         return len(hash)
-# [[6,15],[6,17], [13,20],]
-# [[1, 13], [13, 15]]
-
-
-
         intervals = sorted(intervals)
-        print(intervals)
-        pq = [(-1,-1)]
+        pq = [(0,0)]
         for (start, end) in intervals:
             (soonest_available, _) = pq[0]
             

c6b6be083de3f746f037048e55d6abeeb3542bce
Groza Ionuț <59391450+gionut@users.noreply.github.com>
2024-03-15 16:08:03 +0200

    Time: 87 ms (7.04%), Space: 20.3 MB (24.71%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,6 +1,7 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals = sorted(intervals)
+        print(intervals)
         pq = [(-1,-1)]
         for (start, end) in intervals:
             (soonest_available, _) = pq[0]
